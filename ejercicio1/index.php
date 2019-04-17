d59178583bbafed0f60ab154698608c509ec42f9
unknown <charlie.lopez@epn.edu.ec>
2019-04-16 19:09:45 -0500

    Agregado ejercicio 1

--- ejercicio1/index.php
+++ ejercicio1/index.php
@@ -4,4 +4,14 @@
  * Mostrar la tabla de multiplicar del 2. Emplear el for, luego el while y por último el do/while.
  * La estructura for permite incrementar una variable de 2 en 2
  */
+<?php
+$num=2;
+echo 'Tabla del 2','<br>';
+for($i=0;$i<=10;$i++){
+$res=$num * $i;
+echo $num,'*',$i,' = ',$res,'<br>';
+}
+
+
+
 ?>
